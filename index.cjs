74188d385c02ad492a6131a4d15dccddbea33f7c
yvzn <mail@ludeo.net>
2024-08-02 09:40:12 +0200

    Sanitize avatar props

--- index.cjs
+++ index.cjs
@@ -6,33 +6,34 @@ const beanheads = require('beanheads');
 const app = express();
 const port = 3000;
 
-const parameters = [
-	"accessory",
-    "body",
-    "circleColor",
-    "clothing",
-    "clothingColor",
-    "eyebrows",
-    "eyes",
-    "facialHair",
-    "graphic",
-    "hair",
-    "hairColor",
-    "hat",
-    "hatColor",
-    "lashes",
-    "lipColor",
-    "mask",
-    "faceMask",
-    "mouth",
-    "skinTone",
-]
+const parameters = {
+	"skinTone": Object.keys(beanheads.theme.colors.skin),
+	"eyes": Object.keys(beanheads.eyesMap),
+	"eyebrows": Object.keys(beanheads.eyebrowsMap),
+	"mouth": Object.keys(beanheads.mouthsMap),
+	"hair": Object.keys(beanheads.hairMap),
+	"facialHair": Object.keys(beanheads.facialHairMap),
+	"clothing": Object.keys(beanheads.clothingMap),
+	"accessory": Object.keys(beanheads.accessoryMap),
+	"graphic": Object.keys(beanheads.graphicsMap),
+	"hat": Object.keys(beanheads.hatMap),
+	"body": Object.keys(beanheads.bodyMap),
+	"hairColor": Object.keys(beanheads.theme.colors.hair),
+	"clothingColor": Object.keys(beanheads.theme.colors.clothing),
+	"circleColor": Object.keys(beanheads.theme.colors.bgColors),
+	"lipColor": Object.keys(beanheads.theme.colors.lipColors),
+	"hatColor": Object.keys(beanheads.theme.colors.clothing),
+	"faceMaskColor": Object.keys(beanheads.theme.colors.clothing),
+	"mask": ["true", "false"],
+	"faceMask": ["true", "false"],
+	"lashes": ["true", "false"],
+}
 
 app.get('/', (req, res) => {
 	res.set('Content-Type', 'image/svg+xml');
 
 	const avatarProps = {};
-	for (const parameter of parameters) {
+	for (const parameter in parameters) {
 		if (isValid(parameter, req.query[parameter])) {
 			avatarProps[parameter] = req.query[parameter];
 		}
@@ -53,5 +54,7 @@ app.listen(port, () => {
 });
 
 function isValid(parameterName, parameterValue) {
-	return Boolean(parameterValue);
+	return parameterValue 
+		&& parameterName in parameters
+		&& parameters[parameterName].includes(parameterValue);
 }
